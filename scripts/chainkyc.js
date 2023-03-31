80b7d70d862de075d1cf77a16fee943ed0db9653
sairaj mote <sairajmote3@gmail.com>
2023-04-01 02:52:56 +0530

    KYC revocation changes
    -- listing approved addresses
    -- revocation is done with selection UI
    -- added search option

--- scripts/chainkyc.js
+++ scripts/chainkyc.js
@@ -19,6 +19,61 @@ function getRef(elementId) {
         }
     }
 }
+let zIndex = 50
+// function required for popups or modals to appear
+function openPopup(popupId, pinned) {
+    zIndex++
+    getRef(popupId).setAttribute('style', `z-index: ${zIndex}`)
+    return getRef(popupId).show({ pinned })
+}
+
+// hides the popup or modal
+function closePopup(options = {}) {
+    if (popupStack.peek() === undefined)
+        return;
+    popupStack.peek().popup.hide(options)
+}
+// displays a popup for asking permission. Use this instead of JS confirm
+const getConfirmation = (title, options = {}) => {
+    return new Promise(resolve => {
+        const { message = '', cancelText = 'Cancel', confirmText = 'OK', danger = false } = options
+        getRef('confirm_title').innerText = title;
+        getRef('confirm_message').innerText = message;
+        const cancelButton = getRef('confirmation_popup').querySelector('.cancel-button');
+        const confirmButton = getRef('confirmation_popup').querySelector('.confirm-button')
+        confirmButton.textContent = confirmText
+        cancelButton.textContent = cancelText
+        if (danger)
+            confirmButton.classList.add('button--danger')
+        else
+            confirmButton.classList.remove('button--danger')
+        const { closed } = openPopup('confirmation_popup')
+        confirmButton.onclick = () => {
+            closePopup({ payload: true })
+        }
+        cancelButton.onclick = () => {
+            closePopup()
+        }
+        closed.then((payload) => {
+            confirmButton.onclick = null
+            cancelButton.onclick = null
+            if (payload)
+                resolve(true)
+            else
+                resolve(false)
+        })
+    })
+}
+// Use when a function needs to be executed after user finishes changes
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+        window.clearTimeout(timeoutId);
+        timeoutId = window.setTimeout(() => {
+            callback.apply(null, args);
+        }, wait);
+    };
+}
 
 class Router {
     constructor(options = {}) {
@@ -83,8 +138,8 @@ function loading(show = true) {
     }
 }
 
-floGlobals.approvedKycAggregators = {};
 function getApprovedAggregators() {
+    floGlobals.approvedKycAggregators = {};
     return new Promise((resolve, reject) => {
         floBlockchainAPI.readAllTxs(floGlobals.masterAddress).then(txs => {
             txs.filter(tx => floCrypto.isSameAddr(tx.vin[0].addr, floGlobals.masterAddress) && tx.floData.startsWith('KYC'))
@@ -113,6 +168,46 @@ function getApprovedAggregators() {
             resolve();
         }).catch(e => {
             console.error(e);
+            reject(e);
+        })
+    })
+}
+
+function getApprovedKycs() {
+    floGlobals.approvedKyc = {};
+    return new Promise((resolve, reject) => {
+        const aggregatorTxs = Object.keys(floGlobals.approvedKycAggregators).map(aggregator => {
+            return floBlockchainAPI.readAllTxs(aggregator);
+        });
+        Promise.all(aggregatorTxs).then(aggregatorData => {
+            aggregatorData = aggregatorData.flat(1)
+                .filter(tx => tx.vin[0].addr in floGlobals.approvedKycAggregators && tx.floData.startsWith('KYC'))
+                .sort((a, b) => a.time - b.time);
+            for (const tx of aggregatorData) {
+                const { floData, time, vin, vout } = tx;
+                const [service, operationType, operationData, validity] = floData.split('|');
+                switch (operationType) {
+                    case 'APPROVE_KYC':
+                        operationData.split(',').forEach(address => {
+                            floGlobals.approvedKyc[address] = {
+                                validFrom: time * 1000,
+                                validTo: validity || Date.now() + 10000000,
+                                verifiedBy: vin[0].addr
+                            };
+                        });
+                        break;
+                    case 'REVOKE_KYC':
+                        operationData.split(',').forEach(address => {
+                            floGlobals.approvedKyc[address].validTo = time * 1000;
+                            floGlobals.approvedKyc[address].revokedBy = vin[0].addr;
+                        });
+                        break;
+                    default:
+                        return;
+                }
+            }
+            resolve();
+        }).catch(e => {
             reject(e);
         })
     })
