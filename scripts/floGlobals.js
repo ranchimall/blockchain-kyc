6cc13901e2a06040aa356560a5d85bffaa98a72e
sairaj mote <sairajmote3@gmail.com>
2023-04-09 15:43:43 +0530

    Changed master address

--- scripts/floGlobals.js
+++ scripts/floGlobals.js
@@ -1,6 +1,6 @@
 const floGlobals = {
     blockchain: "FLO",
     adminID: "FKAEdnPfjXLHSYwrXQu377ugN4tXU7VGdf",
-    application: "TEST_MODE",
-    masterAddress: 'FHDqCJUGdHKHTHdLa8wuQ9NKXNNawfoDJ6'
+    application: "KYC",
+    masterAddress: 'FUT2sV3EJiLGgoLmpXW9jAFuhmWy35V5G6'
 }
